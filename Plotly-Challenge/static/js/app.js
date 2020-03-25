b23d2548c23137bcf6288316d50526935a5b1075
Nazaninazar <54868601+Nazaninazar@users.noreply.github.com>
2020-03-24 20:58:11 -0400

    Update

--- Plotly-Challenge/static/js/app.js
+++ Plotly-Challenge/static/js/app.js
@@ -1,105 +1,112 @@
-import os
+function buildMetadata(sample) {
 
-import pandas as pd
-import numpy as np
-
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
-
-from flask import Flask, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-
-
-#################################################
-# Database Setup
-#################################################
-
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', '') or "sqlite:///db/bellybutton.sqlite"
-db = SQLAlchemy(app)
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(db.engine, reflect=True)
-
-# Save references to each table
-Samples_Metadata = Base.classes.sample_metadata
-Samples = Base.classes.samples
-
-
-@app.route("/")
-def index():
-    """Return the homepage."""
-    return render_template("index.html")
-
-
-@app.route("/names")
-def names():
-    """Return a list of sample names."""
-
-    # Use Pandas to perform the sql query
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
-
-    # Return a list of the column names (sample names)
-    return jsonify(list(df.columns)[2:])
-
-
-@app.route("/metadata/<sample>")
-def sample_metadata(sample):
-    """Return the MetaData for a given sample."""
-    sel = [
-        Samples_Metadata.sample,
-        Samples_Metadata.ETHNICITY,
-        Samples_Metadata.GENDER,
-        Samples_Metadata.AGE,
-        Samples_Metadata.LOCATION,
-        Samples_Metadata.BBTYPE,
-        Samples_Metadata.WFREQ,
-    ]
-
-    results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
-
-    # Create a dictionary entry for each row of metadata information
-    sample_metadata = {}
-    for result in results:
-        sample_metadata["sample"] = result[0]
-        sample_metadata["ETHNICITY"] = result[1]
-        sample_metadata["GENDER"] = result[2]
-        sample_metadata["AGE"] = result[3]
-        sample_metadata["LOCATION"] = result[4]
-        sample_metadata["BBTYPE"] = result[5]
-        sample_metadata["WFREQ"] = result[6]
-
-    print(sample_metadata)
-    return jsonify(sample_metadata)
-
-
-@app.route("/samples/<sample>")
-def samples(sample):
-    """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
-
-    # Filter the data based on the sample number and
-    # only keep rows with values above 1
-    sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
-
-    # Sort by sample
-    sample_data.sort_values(by=sample, ascending=False, inplace=True)
-
-    # Format the data to send as json
-    data = {
-        "otu_ids": sample_data.otu_id.values.tolist(),
-        "sample_values": sample_data[sample].values.tolist(),
-        "otu_labels": sample_data.otu_label.tolist(),
-    }
-    return jsonify(data)
-
-
-if __name__ == "__main__":
-    app.run()
+    // Use `d3.json` to fetch the metadata for a sample
+    var metadataURL = "/metadata/" + sample;
+    // Use d3 to select the panel with id of `#sample-metadata`
+    var panelMetadata = d3.select("#sample-metadata");
+    // Use `.html("") to clear any existing metadata
+    panelMetadata.html("");
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+    d3.json(metadataURL).then(function (data) {
+      Object.entries(data).forEach(([key, value]) => {
+        panelMetadata.append("h5").text(`${key}: ${value}`);
+      });
+      
+      // BONUS: Build the Gauge Chart
+      // buildGauge(data.WFREQ);
+  
+      var data = [{domain: {x: [0, 1], y: [0, 1]}, value: data.WFREQ,
+      title: {text: "Belly Button Washing Frequency Scrubs Per Week", font: {size: 14}},
+      type: "indicator", mode: "gauge+number+delta",
+      delta: {reference: 9, increasing: {color: "green"}},
+      gauge:
+        {axis: {range: [0, 10]}, steps: [{range: [0, 5], color: "lightgray"},
+        {range: [5, 8], color: "gray"}], threshold: {line: {color: "red", width: 4},
+        thickness: 0.75, value: 9}}}];
+  
+      var gaugeLayout = {width: 400, height: 500, margin: {t: 0, b: 0}};
+      Plotly.newPlot("gauge", data, gaugeLayout);
+  
+    });
+  
+  }
+  
+  function buildCharts(sample) {
+  
+    // Use `d3.json` to fetch the sample data for the plots
+    var chartsURL = "/samples/" + sample;
+    d3.json(chartsURL).then((data) => {
+      // Build a Bubble Chart using the sample data
+      var trace1 = {
+        x: data.otu_ids,
+        y: data.sample_values,
+        mode: 'markers',
+        text: data.otu_labels,
+        marker: {
+          color: data.otu_ids,
+          size: data.sample_values,
+          colorscale: "Earth"
+        }
+      };
+      var trace1 = [trace1];
+      var layout = {
+        title: "OTU ID",
+        showlegend: false,
+        height: 600,
+        width: 1500
+      };
+      Plotly.newPlot("bubble", trace1, layout);
+      
+      // Build a Pie Chart
+      // Use slice() to grab the top 10 sample_values,
+      // otu_ids, and labels (10 each).
+      var trace2 = [{
+        values: data.sample_values.slice(0, 10),
+        labels: data.otu_ids.slice(0, 10),
+        hovertext: data.otu_labels.slice(0, 10),
+        type: "pie",
+        marker: {
+          colorscale: "Earth"
+        }
+      }];
+      var layout2 = {
+        showlegend: true,
+        height: 400,
+        width: 500
+      };
+      Plotly.newPlot("pie", trace2, layout2);
+  
+    })
+  }
+  
+  function init() {
+    // Grab a reference to the dropdown select element
+    var selector = d3.select("#selDataset");
+  
+    // Use the list of sample names to populate the select options
+    d3.json("/names").then((sampleNames) => {
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  
+      // Use the first sample from the list to build the initial plots
+      const firstSample = sampleNames[0];
+      buildCharts(firstSample);
+      buildMetadata(firstSample);
+      console.log(firstSample)
+    });
+  }
+  
+  function optionChanged(newSample) {
+    // Fetch new data each time a new sample is selected
+    buildCharts(newSample);
+    buildMetadata(newSample);
+  }
+  
+  // Initialize the dashboard
+  init();
